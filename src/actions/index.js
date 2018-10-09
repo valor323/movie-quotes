ca28623fef767e411438f197bf74c37974817e22
valor323 <caine101@mail.chapman.edu>
2018-10-09 14:14:55 -0700

    initial sign in page

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,20 @@ export const signup = (userInfo) => {
     return async (dispatch) => {
         try {
             const response = await axios.post('http://api.reactprototypes.com/signup', userInfo)
+
             console.log('sign up response', response);
         }catch(error){
             console.log('sign up error', error.message);
         }
+    }
+};
+
+export const signIn = userInfo => async dispatch => {
+    try {
+        const response = await axios.post('http://api.reactprototypes.com/signin', userInfo);
+
+        console.log('sign in response', response);
+    }catch(error){
+        console.log('sign in error');
     }
 };
--- src/components/sign_in.js
+++ src/components/sign_in.js
@@ -1,13 +1,56 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {renderInput} from '../helpers';
+import {connect} from 'react-redux';
+import {signIn} from '../actions';
+
 
 class SignIn extends Component{
+    userSignIn = (values) => {
+        console.log('user sign In info', values);
+        this.props.signIn(values);
+
+    };
     render(){
+        const {handleSubmit} = this.props;
+
         return (
             <div>
                 <h1 className="center">Sign In!</h1>
+                <form onSubmit={handleSubmit(this.userSignIn)}>
+                    <Field name='email' label = 'Email' component={renderInput}/>
+                    <Field name='password' label='Password' type='password' component={renderInput}/>
+                    <div className="row">
+                        <div className="col s12 right-align">
+                            <button className='btn blue lighten-3'>Sign In</button>
+                        </div>
+                    </div>
+                </form> 
             </div>
         )
     }
 };
 
-export default SignIn;
+function validate(values){
+    const {email, password} = values;
+    const error = {};
+
+    if(!email){
+        error.email = 'Please enter your email address.'
+    }
+    if(!password){
+        error.password = 'Please enter a valid password'
+    }
+
+    return error;
+
+};
+
+SignIn =  reduxForm({
+    form: 'sign-in',
+    validate: validate
+})(SignIn);
+
+export default connect(null, {
+    signIn: signIn
+})(SignIn)
